9ddc19e3eb5d9cd45bb8c5983e0cb69c7c38e04c
aditya365 <aditya365.mangalampalli@gmail.com>
2019-08-23 10:22:28 +0530

    changed build scripts

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import app from "./app";
 
 //port on which app runs
-const port = process.env.port ||  3000;
+const port = process.env.PORT ||  3000;
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
